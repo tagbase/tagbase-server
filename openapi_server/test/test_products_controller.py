d64cb9f78686d7d4da0d7c351a1f16904824685b
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2019-07-08 14:55:59 -0700

    Fix issue with Timzone and update from Swagger 2.0 to OpenAPI 3.0.0

--- openapi_server/test/test_products_controller.py
+++ openapi_server/test/test_products_controller.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
 from __future__ import absolute_import
+import unittest
 
 from flask import json
 from six import BytesIO
 
-from swagger_server.models.error import Error  # noqa: E501
-from swagger_server.models.success import Success  # noqa: E501
-from swagger_server.test import BaseTestCase
+from openapi_server.models.error import Error  # noqa: E501
+from openapi_server.models.success import Success  # noqa: E501
+from openapi_server.test import BaseTestCase
 
 
 class TestProductsController(BaseTestCase):
@@ -18,11 +19,15 @@ class TestProductsController(BaseTestCase):
 
         Get eTUFF file and execute ingestion.
         """
-        query_string = [('dmas_granule_id', 8.14),
+        query_string = [('granule_id', 'granule_id_example'),
                         ('file', 'file_example')]
+        headers = { 
+            'Accept': 'application/json',
+        }
         response = self.client.open(
-            '/v1/tagbase/ingest/etuff',
+            '/v2/tagbase/ingest/etuff',
             method='GET',
+            headers=headers,
             query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
@@ -32,16 +37,20 @@ class TestProductsController(BaseTestCase):
 
         Get X file and execute specific profile ingestion.
         """
-        query_string = [('profile', 'profile_example'),
-                        ('file', 'file_example')]
+        query_string = [('source_file_path', 'source_file_path_example'),
+                        ('source_ingest_file', 'source_ingest_file_example'),
+                        ('profile', 'profile_example')]
+        headers = { 
+            'Accept': 'application/json',
+        }
         response = self.client.open(
-            '/v1/tagbase/ingest/ncingester',
+            '/v2/tagbase/ingest/ncingester',
             method='GET',
+            headers=headers,
             query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
-    import unittest
     unittest.main()
