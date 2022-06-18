36ff4393843738ef675fac70d82b007c0a5ea7a5
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2022-06-18 12:42:23 -0700

    ISSUE-111 Refactor unpacking and processing of compressed binary formats

--- tagbase_server/tagbase_server/utils/io_utils.py
+++ tagbase_server/tagbase_server/utils/io_utils.py
@@ -1,7 +1,10 @@
+import glob
 import logging
 import os
 import re
 import tempfile
+from time import time
+from pyunpack import Archive
 from urllib.request import urlopen
 
 logger = logging.getLogger(__name__)
@@ -11,6 +14,9 @@ def process_get_input_data(file):
     """
     Capable of acquiring data over HTTP, HTTPS, FTP and FILE protocols.
     Data not already on the filesystem is saved to /tmp.
+    No explicit cleanup is performed per-se however details of the implementation
+    can be found in https://docs.python.org/3/library/tempfile.html#tempfile.TemporaryFile
+    All input is expected to have utf-8 encoding.
 
     :param file: The data file requested through the GET operation.
     :type file: str
@@ -20,13 +26,15 @@ def process_get_input_data(file):
     local_data_file = data_file[
         re.search(r"[file|ftp|http|https]://[^/]*", data_file).end() :
     ]
-    logger.info(f"Acquiring: {local_data_file}")
     if os.path.isfile(local_data_file):
         data_file = local_data_file
+        logger.info("Found '%s' on filesystem.", local_data_file)
     else:
-        # Download data file
+        logger.info("Acquiring '%s' from remote source.", data_file)
         filename = tempfile.TemporaryFile(
-            dir="/tmp/" + data_file[data_file.rindex("/") + 1 :], mode='"w+"'
+            dir="/tmp/" + data_file[data_file.rindex("/") + 1 :],
+            mode='"w+"',
+            encoding="utf-8",
         )
         response = urlopen(data_file)
         chunk_size = 16 * 1024
@@ -38,5 +46,30 @@ def process_get_input_data(file):
                 f.write(chunk)
 
         data_file = filename
-    submission_filename = data_file[data_file.rindex("/") + 1 :]
-    return data_file, submission_filename
+    return data_file
+
+
+def unpack_compressed_binary(file):
+    """
+    Unpack a variety of compressed input files storing all .txt contents to /tmp.
+    No explicit cleanup is performed per-se however details of the implementation
+    can be found in https://docs.python.org/3/library/tempfile.html#tempfile.TemporaryFile
+    For a list of files which can be processed, see
+    https://pypi.org/project/patool/, and
+    https://github.com/ponty/pyunpack
+
+    :param file: The .zip or .tar.gz file containing one or more eTUFF .txt files.
+    :type file: str
+    """
+    etuff_files = []
+    # temp_dir = tempfile.TemporaryDirectory(dir="/tmp/")
+    temp_dir = "/tmp/" + str(time())
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
+    logger.info("Unpacking %s archive to %s", file, temp_dir)
+    Archive(file).extractall(temp_dir)
+    for root, dirs, files in os.walk(temp_dir):
+        for file in files:
+            if file.endswith(".txt"):
+                etuff_files.append(os.path.join(root, file))
+    return etuff_files
