11c416d4eaa7b5711540769adfe89c2157398080
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2022-07-03 16:45:15 -0700

    Add log stream to stdout

--- tagbase_server/tagbase_server/__main__.py
+++ tagbase_server/tagbase_server/__main__.py
@@ -18,9 +18,9 @@ logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-# s_handler = logging.StreamHandler()
-# s_handler.setFormatter(formatter)
-# logger.addHandler(s_handler)
+s_handler = logging.StreamHandler()
+s_handler.setFormatter(formatter)
+logger.addHandler(s_handler)
 
 rf_handler = RotatingFileHandler(
     f"./logs/{LOGGER_NAME}_log.txt", mode="a", maxBytes=100000, backupCount=10
@@ -33,8 +33,6 @@ app.app.json_encoder = encoder.JSONEncoder
 app.add_api(
     "openapi.yaml",
     arguments={"title": "tagbase-server API"},
-    # base_path='api',
     pythonic_params=True,
     strict_validation=True,
 )
-# app.run(host="0.0.0.0", port=5433)
