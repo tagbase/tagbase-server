8fcbfa83a8d24b76b138eccdb66c8af3c1fe59a6
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2022-11-14 22:26:20 -0800

    ISSUE-135 Implement auto-update mechanism for eTagMetadataInventory.csv and eTUFF-ObservationTypes.csv

--- services/docker-cron/poll_metadata_and_obs_types.py
+++ services/docker-cron/poll_metadata_and_obs_types.py
@@ -1,7 +1,6 @@
 import configparser
-import logging
 import os
-import psycopg
+import psycopg2
 import requests
 
 
@@ -9,30 +8,33 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 
-def pull_resource(key=None, param=None):
+def pull_resource(connection=None, key=None, param=None):
+    """
+
+    :param connection: An instantiated connection to TagbaseDB.
+    :param key: The key to lookup in config.ini
+    :param param: The value to lookup in config.ini
+    """
     r = requests.get(config.get(key, param))
-    with open(param + '.csv', 'w') as fd:
-        fd.write(r.text)
-    fd.close()
-    conn.autocommit = True
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO submission (tag_id, filename, date_time, notes, solution_id) "
-                "VALUES ((SELECT COALESCE(MAX(tag_id), NEXTVAL('submission_tag_id_seq')) "
-                "FROM submission WHERE filename = %s), %s, %s, %s, %s)",
-                (
-                    submission_filename,
-                    submission_filename,
-                    dt.now(tz=pytz.utc).astimezone(get_localzone()),
-                    notes,
-                    solution_id,
-                ),
-            )
-            logger.info(
-                "Successful INSERT of '%s' into 'submission' table.",
-                submission_filename,
-            )
+    file_name = param + '.csv'
+    with open(file_name, 'w') as f:
+        f.write(r.text)
+    f.close()
+    with connection:
+        with connection.cursor() as cur:
+            #cur.execute(f"TRUNCATE {param};")
+            f = open(file_name, "r", encoding="utf-8")
+            if param == "metadata_types":
+                cur.copy_from(f, param, sep=",",
+                              columns=("attribute_id", "category", "attribute_name",
+                                       "description", "example", "comments", "necessity"))
+            else:
+                # must be observation_types
+                cur.copy_from(f, param, sep=",",
+                              columns=("variable_id", "variable_name", "standard_name",
+                                       "variable_source", "variable_units", "notes", "standard_units"))
+            f.close()
+            print("Successfully updated '%s' table.", param)
 
 
 def connect():
@@ -41,27 +43,27 @@ def connect():
     :rtype: connection
     """
     try:
-        tbc_connect = psycopg.connect(
+        tbc_connect = psycopg2.connect(
             dbname="tagbase",
             user="tagbase",
             host="postgres",
             port=os.getenv("POSTGRES_PORT"),
             password=os.getenv("POSTGRES_PASSWORD"),
         )
-    except psycopg.OperationalError as poe:
+    except psycopg2.OperationalError as poe:
         print("Error connecting to DB: {}.", poe)
     return tbc_connect
 
 
-def get_obs():
-    return pull_resource('DATA', 'observation_types')
+def get_metadata(connection):
+    return pull_resource(connection, 'DATA', 'metadata_types')
 
 
-def get_metadata():
-    return pull_resource('DATA', 'metadata_inventory')
+def get_obs(connection=None):
+    return pull_resource(connection, 'DATA', 'observation_types')
 
 
 if __name__ == "__main__":
     conn = connect()
-    pull_resource(get_obs(), conn)
-    pull_resource(get_metadata(), conn)
+    get_metadata(conn)
+    get_obs(conn)
