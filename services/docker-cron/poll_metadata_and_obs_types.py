74f03baccd919dd41a6c37da2af863710b5973af
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2022-12-05 02:08:11 -0800

    ISSUE-135 Implement auto-update mechanism for eTagMetadataInventory.csv and eTUFF-ObservationTypes.csv

--- services/docker-cron/poll_metadata_and_obs_types.py
+++ services/docker-cron/poll_metadata_and_obs_types.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import configparser
 import os
+import pandas as pd
 import psycopg2
 import requests
+
+from io import StringIO
 
 
 config = configparser.ConfigParser()
@@ -13,53 +16,58 @@ config.read(configfile)
 
 def pull_resource(connection=None, key=None, param=None):
     """
-
     :param connection: An instantiated connection to TagbaseDB.
     :param key: The key to lookup in config.ini
     :param param: The value to lookup in config.ini
     """
-    r = requests.get(config.get(key, param))
-    file_name = param + ".csv"
-    with open(file_name, "w") as f:
-        f.write(r.text)
-    f.close()
+    col_names = []
+    if param == "metadata_types":
+        col_names=[
+            "attribute_id",
+            "category",
+            "attribute_name",
+            "description",
+            "example",
+            "comments",
+            "necessity"]
+    else:
+        # must be observation_types
+        col_names=[
+            "variable_id",
+            "variable_name",
+            "standard_name",
+            "variable_source",
+            "variable_units",
+            "notes",
+            "standard_unit"]
+    df = pd.read_csv(config.get(key, param), names=col_names, 
+        encoding="utf-8", 
+        on_bad_lines="error", 
+        quotechar='"', 
+        header=0, 
+        index_col=False)
+
     with connection:
         with connection.cursor() as cur:
-            # cur.execute(f"TRUNCATE {param};")
-            f = open(file_name, "r", encoding="utf-8")
-            if param == "metadata_types":
-                cur.copy_from(
-                    f,
-                    param,
-                    sep=",",
-                    columns=(
-                        "attribute_id",
-                        "category",
-                        "attribute_name",
-                        "description",
-                        "example",
-                        "comments",
-                        "necessity",
-                    ),
-                )
-            else:
-                # must be observation_types
-                cur.copy_from(
-                    f,
-                    param,
-                    sep=",",
-                    columns=(
-                        "variable_id",
-                        "variable_name",
-                        "standard_name",
-                        "variable_source",
-                        "variable_units",
-                        "notes",
-                        "standard_units",
-                    ),
-                )
-            f.close()
-            print("Successfully updated '%s' table.", param)
+            try:
+                cur.execute(f"TRUNCATE TABLE {param} CASCADE;")
+                connection.commit()
+                print("Successfully truncated table:", param)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print("Error truncating table:", param)
+                conn.rollback()
+                return 1
+            buffer = StringIO()
+            df.to_csv(buffer, columns=col_names, header=False, index=False)
+            buffer.seek(0)
+            try:
+                cur.copy_from(buffer, param, sep=",", null="NaN")
+                connection.commit()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print("Error writing data to table:", param, error)
+                conn.rollback()
+                return 1
+            print("Successfully updated table:", param)
 
 
 def connect():
