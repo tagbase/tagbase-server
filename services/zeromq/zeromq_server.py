3cd32188f09a28d3148898d65ef79e7719b3eca0
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2023-02-06 21:23:40 -0800

    ISSUE-174 Create events_log table

--- services/zeromq/zeromq_server.py
+++ services/zeromq/zeromq_server.py
@@ -1,20 +1,21 @@
 import signal
 import zmq
 
-logging.basicConfig(filename='subscriber.log', level=logging.INFO)
+logging.basicConfig(filename="subscriber.log", level=logging.INFO)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 context = zmq.Context()
 
 socket = context.socket(zmq.SUB)
-socket.connect('tcp://localhost:5555')
-socket.setsockopt(zmq.SUBSCRIBE, b'event_log')
+socket.connect("tcp://localhost:5555")
+socket.setsockopt(zmq.SUBSCRIBE, b"event_log")
 
 
 def process_topic(topic=None):
     import db_utils.py
-    if topic is 'event_log/create':
+
+    if topic is "event_log/create":
         db_utils.create_event(
             event_category=msg_parts[0],
             event_id=msg_parts[1],
@@ -32,10 +33,14 @@ def process_topic(topic=None):
             time_end=msg_parts[5],
         )
 
+
 while True:
     message = socket.recv()
     topic, messagedata = string.split()
-    msg_parts = messagedata.split(' ')
-    logging.info('topic: {} message: {} - {}'.format(topic, messagedata, time.strftime("%Y-%m-%d %H:%M")))
+    msg_parts = messagedata.split(" ")
+    logging.info(
+        "topic: {} message: {} - {}".format(
+            topic, messagedata, time.strftime("%Y-%m-%d %H:%M")
+        )
+    )
     process_topic(topic)
-    
