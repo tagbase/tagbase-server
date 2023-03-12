a0aeccbbcdafd0e793b4d74d768288ab992ed70a
Renato Marroquin <marenato@inf.ethz.ch>
2023-03-12 23:06:53 +0100

    Specify fswatch events

--- services/fswatch/post.sh
+++ services/fswatch/post.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
+PATH_TO_CHECK=/usr/src/app/staging_data
 while true
   do
-    fswatch --one-event /usr/src/app/staging_data | while read line
+    fswatch --one-event $PATH_TO_CHECK --event Created --event MovedTo --event IsFile | while read line
       do
       	filename="${line##*/}"
-        echo "Contents of /usr/src/app/staging_data changed; Processing: $line"
+        echo "Contents of $PATH_TO_CHECK changed; Processing: $line"
         curl -X 'POST' tagbase_server:5433/tagbase/api/v0.8.0/ingest?filename="$filename" -H 'accept: application/json' -T $line
       done
   done
