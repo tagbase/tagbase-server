99ebda2bc21b071d086d5fd06614017322bc7f14
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2023-04-05 23:38:34 -0700

    ISSUE-211 fswatch needs to account for empty files created by sftp clients

--- services/fswatch/post.sh
+++ services/fswatch/post.sh
@@ -2,16 +2,23 @@
 PATH_TO_CHECK=/usr/src/app/staging_data/
 while true
   do
-    #fswatch --one-event $PATH_TO_CHECK --event Created --event MovedTo --event IsFile --event Updated | while read line
-    #fswatch --one-event $PATH_TO_CHECK | while read line
-    fswatch --batch-marker=EOF --event-flags --numeric --event Updated --event IsFile $PATH_TO_CHECK | while read file event; do
-      echo $file $event
-      if [ $file = "EOF" ]; then 
-        #do
-          #filename="${file##*/}"
-        echo "Contents of $PATH_TO_CHECK changed; Processing: $file"
-          # curl -X 'POST' tagbase_server:5433/tagbase/api/v0.9.0/ingest?filename="$filename" -H 'accept: application/json' -T $line
-        #done
-      fi
-    done
-  done
+    fswatch --one-event $PATH_TO_CHECK --event Created --event MovedTo --event IsFile --event Updated | while read line
+      do
+        filename="${line##*/}"
+        echo "Contents of $PATH_TO_CHECK changed; Checking: $filename"
+
+        # waiting for file to be fully uploaded
+        while true
+          do
+            size_bfr=$(stat -c%s "$line")
+            sleep 0.5
+            size_aftr=$(stat -c%s "$line")
+            if [ $size_bfr -eq $size_aftr ];
+            then
+                break;
+            fi
+          done
+
+        echo "Processing: $filename"
+        curl -X 'POST' tagbase_server:5433/tagbase/api/v0.9.0/ingest?filename="$filename" -H 'accept: application/json' -T $line
+      done
