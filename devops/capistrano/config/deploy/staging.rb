3cbdd03b3d9febc4a640052464ea71c18919dcfb
Renato Marroquin <marenato@inf.ethz.ch>
2023-04-06 06:42:42 +0200

    Update deploy branch

--- devops/capistrano/config/deploy/staging.rb
+++ devops/capistrano/config/deploy/staging.rb
@@ -1,5 +1,5 @@
-set :branch, "149_configuration_tool"
-set :server_ip, "3.15.0.82"
+set :branch, "main" #149_configuration_tool"
+set :server_ip, "18.118.1.177"# 3.15.0.82"
 
 # server-based syntax
 # ======================
