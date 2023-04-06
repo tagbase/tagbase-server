40d386daf91d6c48890e1b09b349aac54f061be7
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2023-04-05 22:28:46 -0700

    ISSUE-211 fswatch needs to account for empty files created by sftp clients

--- services/fswatch/post.sh
+++ services/fswatch/post.sh
@@ -2,11 +2,15 @@
 PATH_TO_CHECK=/usr/src/app/staging_data/
 while true
   do
-    fswatch --one-event $PATH_TO_CHECK --event Created --event MovedTo --event IsFile --event Updated | while read line
+    #fswatch --one-event $PATH_TO_CHECK --event Created --event MovedTo --event IsFile --event Updated | while read line
     #fswatch --one-event $PATH_TO_CHECK | while read line
-      do
-      	filename="${line##*/}"
-        echo "Contents of $PATH_TO_CHECK changed; Processing: $line"
-        curl -X 'POST' tagbase_server:5433/tagbase/api/v0.9.0/ingest?filename="$filename" -H 'accept: application/json' -T $line
-      done
+    fswatch --batch-marker=EOF -xn $PATH_TO_CHECK | while read file event; do
+      echo $file $event
+      if [ $file = "EOF" ]; then 
+        do
+          #filename="${file##*/}"
+          echo "Contents of $PATH_TO_CHECK changed; Processing: $file"
+          # curl -X 'POST' tagbase_server:5433/tagbase/api/v0.9.0/ingest?filename="$filename" -H 'accept: application/json' -T $line
+        done
+      fi
   done
