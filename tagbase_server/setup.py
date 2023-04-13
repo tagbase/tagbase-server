875e2c864216cb1dd80d5e45031d8c3cd3b96244
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2023-04-12 23:39:12 -0700

    Prep for v0.10.1 release

--- tagbase_server/setup.py
+++ tagbase_server/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "tagbase_server"
-VERSION = "v0.10.0"
+VERSION = "v0.10.1"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
