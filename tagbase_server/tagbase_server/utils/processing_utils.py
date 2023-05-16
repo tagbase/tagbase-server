325c95c8df3d928d43cc3b8315746318a5819bae
Renato Marroquin <marenato@inf.ethz.ch>
2023-05-16 08:12:05 +0200

    239: Revert logic + add unit test

--- tagbase_server/tagbase_server/utils/processing_utils.py
+++ tagbase_server/tagbase_server/utils/processing_utils.py
@@ -8,7 +8,7 @@ import psycopg2.extras
 import pytz
 from tzlocal import get_localzone
 
-from tagbase_server.utils.db_utils import connect, detect_duplicate
+from tagbase_server.utils.db_utils import connect
 from tagbase_server.utils.io_utils import compute_sha256
 from tagbase_server.utils.slack_utils import post_msg
 
@@ -128,6 +128,39 @@ def insert_metadata(cur, tag_id, metadata):
     logger.debug("metadata: %s", metadata)
 
 
+def detect_duplicate(cursor, hash_sha256):
+    """
+    Detect a duplicate file by performing a lookup on submission SHA256 hash.
+    Returns True if duplicate.
+
+    :param hash_sha256: A SHA256 hash.
+    :type hash_sha256: str
+
+    :param cursor: A database cursor
+    :type cursor: cursor connection
+    """
+    logger.info("Detecting duplicate...")
+    cursor.execute(
+        "SELECT hash_sha256 FROM submission WHERE hash_sha256 = %s",
+        (hash_sha256,),
+    )
+    db_results = cursor.fetchone()
+
+    if not db_results:
+        return False
+    duplicate = db_results[0]
+
+    logger.info(
+        "Computed hash: %s Duplicate: %s",
+        hash_sha256,
+        duplicate,
+    )
+    if duplicate is not None:
+        return True
+    else:
+        return False
+
+
 def process_etuff_file(file, version=None, notes=None):
     start = time.perf_counter()
     submission_filename = file  # full path name is now preferred rather than - file[file.rindex("/") + 1 :]
@@ -135,202 +168,201 @@ def process_etuff_file(file, version=None, notes=None):
         "Processing etuff file: %s",
         submission_filename,
     )
-    hash_sha256 = compute_sha256(submission_filename)
-    logger.info("Detecting duplicate...")
-    duplicate = detect_duplicate(hash_sha256)
-    logger.info(
-        "Duplicate: %s",
-        duplicate,
-    )
 
-    if duplicate:
-        conn = connect()
-        conn.autocommit = True
-        with conn:
-            with conn.cursor() as cur:
-                # get tag_id based on max tag_id value found in the submission table
-                tag_id = get_tag_id(cur, submission_filename)
+    conn = connect()
+    conn.autocommit = True
 
-                submission_id = insert_new_submission(
-                    cur, tag_id, submission_filename, notes, version, hash_sha256
+    with conn:
+        with conn.cursor() as cur:
+            hash_sha256 = compute_sha256(submission_filename)
+            if detect_duplicate(cur, hash_sha256):
+                logger.info(
+                    "Data file '%s' with SHA256 hash '%s' identified as duplicate. No ingestion performed.",
+                    submission_filename,
+                    hash_sha256,
                 )
-                logger.debug(
-                    "Working with submission_id={} tag_id={}".format(
-                        submission_id, tag_id
-                    )
+                return 1
+
+            # get tag_id based on max tag_id value found in the submission table
+            tag_id = get_tag_id(cur, submission_filename)
+
+            submission_id = insert_new_submission(
+                cur, tag_id, submission_filename, notes, version, hash_sha256
+            )
+            logger.debug(
+                "Working with submission_id={} tag_id={}".format(
+                    submission_id, tag_id
                 )
+            )
 
-                metadata = []
-                proc_obs = []
+            metadata = []
+            proc_obs = []
 
-                s_time = time.perf_counter()
-                with open(file, mode="rb") as data:
-                    line_counter = 0
-                    variable_lookup = {}
+            s_time = time.perf_counter()
+            with open(file, mode="rb") as data:
+                line_counter = 0
+                variable_lookup = {}
 
-                    global_attributes, metadata_lines = _get_global_attributes(data)
-                    process_all_lines_for_global_attributes(
-                        global_attributes,
-                        cur,
-                        submission_id,
-                        metadata,
-                        submission_filename,
-                    )
-                    line_counter += metadata_lines
+                global_attributes, metadata_lines = _get_global_attributes(data)
+                process_all_lines_for_global_attributes(
+                    global_attributes,
+                    cur,
+                    submission_id,
+                    metadata,
+                    submission_filename,
+                )
+                line_counter += metadata_lines
 
-                    while line := data.readline():
-                        line_counter += 1
-                        line = line.decode("UTF-8")
-                        tokens = line.split(",")
-                        tokens = [token.replace('"', "") for token in tokens]
-                        if not tokens:
-                            continue
+                while line := data.readline():
+                    line_counter += 1
+                    line = line.decode("UTF-8")
+                    tokens = line.split(",")
+                    tokens = [token.replace('"', "") for token in tokens]
+                    if not tokens:
+                        continue
 
-                        # get variable
-                        variable_name = tokens[3]
-                        if variable_name in variable_lookup:
-                            variable_id = variable_lookup[variable_name]
-                        else:
-                            cur.execute(
-                                "SELECT variable_id FROM observation_types WHERE variable_name = %s",
-                                (variable_name,),
-                            )
-                            row = cur.fetchone()
-                            if row:
-                                variable_id = row[0]
-                            else:
-                                try:
-                                    logger.debug(
-                                        "variable_name=%s\ttokens=%s",
-                                        variable_name,
-                                        tokens,
-                                    )
-                                    cur.execute(
-                                        "INSERT INTO observation_types("
-                                        "variable_name, variable_units) VALUES (%s, %s) "
-                                        "ON CONFLICT (variable_name) DO NOTHING",
-                                        (variable_name, tokens[4].strip()),
-                                    )
-                                except (
-                                    Exception,
-                                    psycopg2.DatabaseError,
-                                ) as error:
-                                    logger.error(
-                                        "variable_id '%s' already exists in 'observation_types'. tokens:"
-                                        " '%s. \nerror: %s",
-                                        variable_name,
-                                        tokens,
-                                        error,
-                                    )
-                                    conn.rollback()
-                                cur.execute(
-                                    "SELECT nextval('observation_types_variable_id_seq')"
-                                )
-                                variable_id = cur.fetchone()[0]
-                            variable_lookup[variable_name] = variable_id
-
-                        # get date from token
-                        date_time = None
-                        if tokens[0] != '""' and tokens[0] != "":
-                            if tokens[0].startswith('"'):
-                                tokens[0].replace('"', "")
-                            date_time = dt.strptime(
-                                tokens[0], "%Y-%m-%d %H:%M:%S"
-                            ).astimezone(pytz.utc)
-                        else:
-                            stripped_line = line.strip("\n")
-                            msg = (
-                                f"*{submission_filename}* _line:{line_counter}_ - "
-                                f"No datetime... skipping line: {stripped_line}"
-                            )
-                            post_msg(msg)
-                            continue
-
-                        proc_obs.append(
-                            [
-                                date_time,
-                                variable_id,
-                                tokens[2],
-                                submission_id,
-                                tag_id,
-                            ]
+                    # get variable
+                    variable_name = tokens[3]
+                    if variable_name in variable_lookup:
+                        variable_id = variable_lookup[variable_name]
+                    else:
+                        cur.execute(
+                            "SELECT variable_id FROM observation_types WHERE variable_name = %s",
+                            (variable_name,),
                         )
+                        row = cur.fetchone()
+                        if row:
+                            variable_id = row[0]
+                        else:
+                            try:
+                                logger.debug(
+                                    "variable_name=%s\ttokens=%s",
+                                    variable_name,
+                                    tokens,
+                                )
+                                cur.execute(
+                                    "INSERT INTO observation_types("
+                                    "variable_name, variable_units) VALUES (%s, %s) "
+                                    "ON CONFLICT (variable_name) DO NOTHING",
+                                    (variable_name, tokens[4].strip()),
+                                )
+                            except (
+                                Exception,
+                                psycopg2.DatabaseError,
+                            ) as error:
+                                logger.error(
+                                    "variable_id '%s' already exists in 'observation_types'. tokens:"
+                                    " '%s. \nerror: %s",
+                                    variable_name,
+                                    tokens,
+                                    error,
+                                )
+                                conn.rollback()
+                            cur.execute(
+                                "SELECT nextval('observation_types_variable_id_seq')"
+                            )
+                            variable_id = cur.fetchone()[0]
+                        variable_lookup[variable_name] = variable_id
 
-                    len_proc_obs = len(proc_obs)
-                    e_time = time.perf_counter()
-                    sub_elapsed = round(e_time - s_time, 2)
-                    logger.info(
-                        "Built raw 'proc_observations' data structure from %s observations in: %s second(s)",
-                        len_proc_obs,
-                        sub_elapsed,
+                    # get date from token
+                    date_time = None
+                    if tokens[0] != '""' and tokens[0] != "":
+                        if tokens[0].startswith('"'):
+                            tokens[0].replace('"', "")
+                        date_time = dt.strptime(
+                            tokens[0], "%Y-%m-%d %H:%M:%S"
+                        ).astimezone(pytz.utc)
+                    else:
+                        stripped_line = line.strip("\n")
+                        msg = (
+                            f"*{submission_filename}* _line:{line_counter}_ - "
+                            f"No datetime... skipping line: {stripped_line}"
+                        )
+                        post_msg(msg)
+                        continue
+
+                    proc_obs.append(
+                        [
+                            date_time,
+                            variable_id,
+                            tokens[2],
+                            submission_id,
+                            tag_id,
+                        ]
                     )
 
-                insert_metadata(cur, tag_id, metadata)
-
-                s_time = time.perf_counter()
-                df = pd.DataFrame.from_records(
-                    proc_obs,
-                    columns=[
-                        "date_time",
-                        "variable_id",
-                        "variable_value",
-                        "submission_id",
-                        "tag_id",
-                    ],
-                )
+                len_proc_obs = len(proc_obs)
                 e_time = time.perf_counter()
                 sub_elapsed = round(e_time - s_time, 2)
                 logger.info(
-                    "Built Pandas DF from %s records. Time elapsed: %s second(s)",
-                    len_proc_obs,
-                    sub_elapsed,
-                )
-                logger.debug("DF Info: %s", df.info)
-                logger.debug("DF Memory Usage: %s", df.memory_usage(True))
-
-                # save dataframe to StringIO memory buffer
-                s_time = time.perf_counter()
-                buffer = StringIO()
-                df.to_csv(buffer, header=False, index=False)
-                buffer.seek(0)
-                e_time = time.perf_counter()
-                sub_elapsed = round(e_time - s_time, 2)
-                logger.info(
-                    "Copied Pandas DF to StringIO memory buffer. Time elapsed: %s second(s)",
-                    sub_elapsed,
-                )
-
-                # copy buffer to db
-                s_time = time.perf_counter()
-                logger.info(
-                    "Copying memory buffer to 'proc_observations' and executing 'data_migration' TRIGGER."
-                )
-                try:
-                    cur.copy_from(buffer, "proc_observations", sep=",")
-                except (Exception, psycopg2.DatabaseError) as error:
-                    logger.error("Error: %s", error)
-                    conn.rollback()
-                    return 1
-                e_time = time.perf_counter()
-                sub_elapsed = round(e_time - s_time, 2)
-                logger.info(
-                    "Successful migration of %s 'proc_observations'. Elapsed time: %s second(s).",
+                    "Built raw 'proc_observations' data structure from %s observations in: %s second(s)",
                     len_proc_obs,
                     sub_elapsed,
                 )
 
-        conn.commit()
+            insert_metadata(cur, tag_id, metadata)
 
-        cur.close()
-        conn.close()
+            s_time = time.perf_counter()
+            df = pd.DataFrame.from_records(
+                proc_obs,
+                columns=[
+                    "date_time",
+                    "variable_id",
+                    "variable_value",
+                    "submission_id",
+                    "tag_id",
+                ],
+            )
+            e_time = time.perf_counter()
+            sub_elapsed = round(e_time - s_time, 2)
+            logger.info(
+                "Built Pandas DF from %s records. Time elapsed: %s second(s)",
+                len_proc_obs,
+                sub_elapsed,
+            )
+            logger.debug("DF Info: %s", df.info)
+            logger.debug("DF Memory Usage: %s", df.memory_usage(True))
 
-        logger.info(
-            "Data file '%s' successfully ingested into Tagbase DB.",
-            submission_filename,
-        )
-    else:
-        logger.info(
-            "Data file '%s' with SHA256 hash '%s' identified as duplicate. No ingestion performed.",
-            submission_filename,
-            hash_sha256,
-        )
+            # save dataframe to StringIO memory buffer
+            s_time = time.perf_counter()
+            buffer = StringIO()
+            df.to_csv(buffer, header=False, index=False)
+            buffer.seek(0)
+            e_time = time.perf_counter()
+            sub_elapsed = round(e_time - s_time, 2)
+            logger.info(
+                "Copied Pandas DF to StringIO memory buffer. Time elapsed: %s second(s)",
+                sub_elapsed,
+            )
+
+            # copy buffer to db
+            s_time = time.perf_counter()
+            logger.info(
+                "Copying memory buffer to 'proc_observations' and executing 'data_migration' TRIGGER."
+            )
+            try:
+                cur.copy_from(buffer, "proc_observations", sep=",")
+            except (Exception, psycopg2.DatabaseError) as error:
+                logger.error("Error: %s", error)
+                conn.rollback()
+                return 1
+            e_time = time.perf_counter()
+            sub_elapsed = round(e_time - s_time, 2)
+            logger.info(
+                "Successful migration of %s 'proc_observations'. Elapsed time: %s second(s).",
+                len_proc_obs,
+                sub_elapsed,
+            )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    finish = time.perf_counter()
+    elapsed = round(finish - start, 2)
+    logger.info(
+        "Data file %s successfully ingested into Tagbase DB. Total time: %s second(s)",
+        submission_filename,
+        elapsed,
+    )
